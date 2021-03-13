ead71c81e394455295da13f4e11228c94cf6ca2c
hemalathakolluru18 <hemalathakolluru18@gmail.com>
2021-03-13 16:30:21 +0530

    add new file

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+/**
+*name:Hemalatha
+*Description:Demo of simple application using variables
+*Date:3/12/2021
+*/
+class Helloworld{
+    public static void main(string[]args){
+	   system.out.println("Hello world");
+	   }
+	   }
+	   
